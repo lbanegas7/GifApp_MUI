757c14826b2326ee1b2c4b98baa9c5f6a40239ed
Luis Banegas <luisbanegas06@gmail.com>
2022-05-11 19:22:51 -0600

    Agregue estilos para dispositivos moviles

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -7,13 +7,6 @@ import CircularProgress from '@mui/material/CircularProgress';
 export const GifGrid = ({ valueSearch }) => {
 
   const { data, loading } = useFetchGifs(valueSearch)
-  console.log(data,loading);
-  // const gifs = [
-  //   {
-  //     url: 'https://bit.ly/dan-abramov',
-  //     title: 'Imagen de prueba'
-  //   }
-  // ]
 
   const progress = () => (
     <Box sx={{ display: 'flex', alignItems: 'center', justifyContent:'center'}}>
@@ -23,7 +16,7 @@ export const GifGrid = ({ valueSearch }) => {
   return (
     <>
       { 
-      loading ? progress() : (<Box sx={{ flexGrow: 1, width: '80%', marginLeft: 'auto', marginRight: 'auto', mb: 5}} >
+      loading ? progress() : (<Box sx={{ flexGrow: 1, width: {xs: '96%', md: '90%', lg: '80%'}, marginLeft: 'auto', marginRight: 'auto', mb: 5}} >
         <Grid container columnSpacing={{ xs: 1, sm: 2, md: 3 }} spacing={2}>
           {
             data.map((gif, position) => (<Grid key={position} item xs={6} sm={4} md={3} >
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,28 @@
 import { Box, IconButton } from '@mui/material'
 import React from 'react'
 import { LogoGifApp } from './LogoGifApp'
-import {FaGithub } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 
 export const Navbar = () => {
   return (
     <Box
       sx={{
-          width: 'auto',
-          height: 70,
-          display:'flex',
-          alignItems: 'center'
-        }}
+        width: 'auto',
+        height: 70,
+        display: 'flex',
+        alignItems: 'center'
+      }}
     >
-        <LogoGifApp />
-        <IconButton 
-        color='primary' 
-        aria-label='repositorio github' 
-        size="large" 
-        sx={{display:'block', marginLeft:'auto'}}>
-            <FaGithub />
-        </IconButton>
+      <LogoGifApp />
+      <IconButton
+        color='primary'
+        aria-label='repositorio github'
+        size="large"
+        sx={{ display: 'block', marginLeft: 'auto' }}>
+        <a href='https://github.com/lbanegas7/GitApp_MUI' target='_blank' rel='noopener noreferrer'>
+          <FaGithub />
+        </a>
+      </IconButton>
     </Box>
   )
 }
